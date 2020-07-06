9d3f10d7c4207b406e1d1d951a1cc8e26b878ba8
André Nogueira <estevo.andr@gmail.com>
2020-07-06 18:32:42 -0300

    adding .vim folder; fixing path for vim ln's

--- vim/install.sh
+++ vim/install.sh
@@ -4,4 +4,6 @@
 
 eval userhome=~$(whoami)
 
-ln -s $PWD"/.vimrc" $userhome"/.vimrc"
+#this is meant to be executed with the installAllDotfiles.sh script on the folder 1 level behind
+ln -s $PWD"/vim/.vimrc" $userhome"/.vimrc"
+ln -s $PWD"/vim/.vim" $userhome"/.vim"
